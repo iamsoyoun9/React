b283278a76461fc804128acb3fdbb8795a0a0a6c
나소영 <iamsoyoun9@naver.com>
2022-11-28 08:59:32 +0900

    state 기초

--- study/src/App.js
+++ study/src/App.js
@@ -1,21 +1,31 @@
+import React, {useState} from 'react';
 import './App.css';
-import logo from './logo.svg'
 
 function App() {
 
+	let [글제목,글제목변경] = useState(['크리스마스 홈파티 와인 추천','크리스마스 홈파티 음식 추천','크리스마스 홈파티 디저트 추천']); // [a,b] : a = 남자코트추천 / b= 새로운 자리?
 	let posts = '상무지구 고기 맛집';
-	function num(){
-		return 100
-	}
 
 	return (
 		<div className="App">
 			<div className='black-nav'>
 				<div>React 공부</div>
 			</div>
-			<img src={logo} alt="" />
-			<h4>{ posts }</h4>
-			<h4>{ num() }</h4>
+			<div className='list'>
+				<h3>{글제목[0]}</h3>
+				<p>11월 28일 발행</p>
+				<hr />
+			</div>
+			<div className='list'>
+				<h3>{글제목[1]}</h3>
+				<p>11월 28일 발행</p>
+				<hr />
+			</div>
+			<div className='list'>
+				<h3>{글제목[2]}</h3>
+				<p>11월 28일 발행</p>
+				<hr />
+			</div>
 		</div>
 	);
 }
